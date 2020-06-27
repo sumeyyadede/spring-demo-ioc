174e09f7dcdce74db6354665fbde5bac82476961
sumeyyadede <dedesumeyya@gmail.com>
2020-06-26 21:18:15 -0400

    added HelloSpringApp, BaseballCoach and GolfCoach classes.

---
+++ spring-demo-ioc/src/com/sumeyyadede/springdemo/GolfCoach.java
@@ -0,0 +1,10 @@
+package com.sumeyyadede.springdemo;
+
+public class GolfCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your putting skills for 2 hours today";
+	}
+	
+}
